033f76da78b5cb584373706794a58e0084d5be04
Yaroslav Pavlinskiy <yarbezl@gmail.com>
2025-04-11 00:38:34 +0300

    feat: initial commit

---
+++ src/api/factories.ts
@@ -0,0 +1,10 @@
+import { faker } from '@faker-js/faker';
+import { User } from '../core/user';
+
+export const createUserPayload = (override: Partial<User> = {}) => {
+    return {
+        email: faker.internet.email(),
+        password: faker.internet.password(),
+        ...override
+    }
+}
---
+++ src/api/users.ts
@@ -0,0 +1,8 @@
+export const mockUserData = {
+    validData: {
+        id: "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+        email: "alice@example.com",
+        password: "hashed_password_1",
+        createdAt: "2024-04-01T12:34:56.789Z"
+      }
+}
