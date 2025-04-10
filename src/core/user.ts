f2dfbce7c5ff106acd2d8960c1e5064a81ea541d
Yaroslav Pavlinskiy <yarbezl@gmail.com>
2025-04-11 00:55:25 +0300

    chore: lint & format

--- src/core/user.ts
+++ src/core/user.ts
@@ -1,6 +1,6 @@
 export type User = {
-    id?: string;
-    email: string;
-    password: string;
-    createdAt?: string;
-} 
+  id?: string;
+  email: string;
+  password: string;
+  createdAt?: string;
+};
