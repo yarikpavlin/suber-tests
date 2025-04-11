c487c0a6db23785f91d9cc6ccf36dbc339e9253f
Yaroslav Pavlinskiy <yarbezl@gmail.com>
2025-04-11 03:13:49 +0300

    test: auth tests
    test: remove POST /user tests

---
+++ tests/api/auth.spec.ts
@@ -0,0 +1,68 @@
+import { test, expect } from '@playwright/test';
+import { createUserPayload } from '../../src/api/factories';
+
+test.describe.serial('@api @auth /auth', () => {
+  test('POST /signup should create a new user and return a token', async ({ request }) => {
+    const userData = createUserPayload();
+    const response = await request.post('/auth/signup', {
+      data: userData,
+    });
+    const token = response.headers()['authorization'];
+
+    expect(response.status()).toBe(201);
+    expect(token).toBeDefined();
+  });
+
+  test('POST /signup should return 409 if user already exists', async ({ request }) => {
+    const userData = createUserPayload();
+    await request.post('/auth/signup', {
+      data: userData,
+    });
+
+    await new Promise((resolve) => setTimeout(resolve, 200));
+
+    const response = await request.post('/auth/signup', {
+      data: userData,
+    });
+    expect(response.status()).toBe(409);
+  });
+
+  test('POST /login should return a token', async ({ request }) => {
+    const userData = createUserPayload();
+    await request.post('/auth/signup', {
+      data: userData,
+    });
+
+    await new Promise((resolve) => setTimeout(resolve, 500));
+
+    const response = await request.post('/auth/login', {
+      data: userData,
+    });
+    const token = response.headers()['authorization'];
+
+    expect(response.status()).toBe(200);
+    expect(token).toBeDefined();
+  });
+
+  test('POST /login should return 401 if user does not exist', async ({ request }) => {
+    const userData = createUserPayload();
+    const response = await request.post('/auth/login', {
+      data: userData,
+    });
+    expect(response.status()).toBe(401);
+  });
+
+  test('POST /login should return 401 if password is incorrect', async ({ request }) => {
+    const userData = createUserPayload();
+    await request.post('/auth/login', {
+      data: userData,
+    });
+
+    await new Promise((resolve) => setTimeout(resolve, 500));
+
+    const response = await request.post('/auth/login', {
+      data: { ...userData, password: 'incorrect' },
+    });
+    expect(response.status()).toBe(401);
+  });
+});
--- tests/api/users.spec.ts
+++ tests/api/users.spec.ts
@@ -1,8 +1,7 @@
 import { test, expect } from '@playwright/test';
 import { mockUserData } from '../../src/api/users';
-import { createUserPayload } from '../../src/api/factories';
 
-test.describe('/users', () => {
+test.describe.serial('/users', () => {
   test('@api GET / positive', async ({ request }, testInfo) => {
     const response = await request.get('/users');
     testInfo.attach(response.url(), {
@@ -22,32 +21,5 @@ test.describe('/users', () => {
   test('@api GET /:id negative', async ({ request }) => {
     const response = await request.get(`/users/123`);
     expect(response.status()).toBe(404);
-  });
-
-  test('@api POST / positive', async ({ request }) => {
-    const newUser = createUserPayload();
-    const response = await request.post('/users', {
-      data: newUser,
-    });
-
-    const returnedUser = await response.json();
-    expect(response.status()).toBe(201);
-    expect({ email: returnedUser.email, password: returnedUser.password }).toEqual(newUser);
-  });
-
-  test('@api POST / negative 409', async ({ request }) => {
-    const newUser = createUserPayload();
-
-    await request.post('/users', {
-      data: newUser,
-    });
-
-    // Add a delay to ensure the first request is fully processed
-    await new Promise((resolve) => setTimeout(resolve, 100));
-
-    const response = await request.post('/users', {
-      data: newUser,
-    });
-    expect(response.status()).toBe(409);
   });
 });
